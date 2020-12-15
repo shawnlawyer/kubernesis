fe87706d8507926ec74d5ad8d3d0a74e83e1250b
Shawn Lawyer <shawnlawyer@gmail.com>
2020-12-15 03:53:27 -0500

    feat: adds ingress

--- launch.sh
+++ launch.sh
@@ -3,17 +3,19 @@
 minikube delete
 minikube start --driver=hyperkit
 screen -dmS minikube_dashboard bash -c 'minikube dashboard; exit;'
+MINIKUBE_URL=http://$(minikube ip)
+minikube addons enable ingress
 eval $(minikube docker-env)
 docker build ./nginx-alpine -t nginx-alpine:1.0 -f nginx-alpine/Dockerfile
 kubectl apply -f k8s
 echo "Cluster Started"
 for i in {1..60}
 do
-    if curl -s --head  --request GET $(minikube service nginx-load-balancer-service --url) | grep "200" > /dev/null
+    if curl -s --head  --request GET ${MINIKUBE_URL} | grep "200" > /dev/null
     then
         echo "Nginx Running"
         sleep 10
-        open $(minikube service nginx-load-balancer-service --url)
+        open ${MINIKUBE_URL}
         break
     fi
     sleep 1
